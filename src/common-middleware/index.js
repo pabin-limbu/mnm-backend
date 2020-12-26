086fed63bb03462b1656775f64617f0bddf7eab8
pabin-limbu <changsu.pabin337@gmail.com>
2020-12-26 21:11:29 +0800

    middleware to authenticate user-sign-in and to check valid user role created in common-middleware /

---
+++ src/common-middleware/index.js
@@ -0,0 +1,31 @@
+const jwt = require("jsonwebtoken");
+
+//middleware to vallidate if user is logged-in
+exports.requireSignin = (req, res, next) => {
+  if (req.headers.authorization) {
+    const token = req.headers.authorization.split(" ")[1];
+    const user = jwt.verify(token, process.env.JWT_SECRET); // conditional verification needed if wrong jwt.
+    //  console.log(user);
+    //req.body.user = user; // decrypt jwt token and attactch user to the req obj.
+    req.user = user; // decrypt jwt token and attactch user to the req obj.
+    next();
+  } else {
+    return res.status(400).json({ message: "authorization required" });
+  }
+};
+
+exports.isUser = (req, res, next) => {
+  if (req.user.role !== "user") {
+    //   console.log(req.body.role);
+    return res.status(400).json({ message: " USER ACCESS DENIED" });
+  }
+  next();
+};
+
+exports.isAdmin = (req, res, next) => {
+  if (req.user.role !== "admin") {
+    //   console.log(req.body.role);
+    return res.status(400).json({ message: " ADMIN ACCESS DENIED" });
+  }
+  next();
+};
